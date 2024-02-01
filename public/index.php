0279ba61adea2f1058b4aca43846c5f0649a8d50
Murilo Elias <muriloelias@ipeweb.com.br>
2024-02-01 11:59:19 -0300

    Fixes

--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
 require_once('../vendor/autoload.php');
 
 use Ipeweb\IpeSheets\Bootstrap\Environments;
-use Ipeweb\IpeSheets\Bootstrap\Helper;
 use Ipeweb\IpeSheets\Bootstrap\Request;
+
+error_reporting(0);
 
 Environments::getEnvironments();
 
