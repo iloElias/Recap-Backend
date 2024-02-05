1ed8b0b0de4f2c40d6280a4f8238e6c5196fbd25
Murilo Elias <muriloelias@ipeweb.com.br>
2024-02-05 15:14:00 -0300

    Last Test

--- public/index.php
+++ public/index.php
@@ -5,6 +5,16 @@ require_once('../vendor/autoload.php');
 use Ipeweb\IpeSheets\Bootstrap\Environments;
 use Ipeweb\IpeSheets\Bootstrap\Request;
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
+
 error_reporting(0);
 
 Environments::getEnvironments();
