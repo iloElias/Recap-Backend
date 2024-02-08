ea5499ff3ec001f6b286b836f1128902e3d9ea5a
iloElias <murilo7456@gmail.com>
2024-02-07 22:00:28 -0300

    Test

--- src/Bootstrap/Request.php
+++ src/Bootstrap/Request.php
@@ -247,6 +247,7 @@ class Request
     {
         try {
             header("Access-Control-Allow-Origin: *");
+            header('Access-Control-Allow-Credentials: true');
             header("Access-Control-Allow-Headers: *");
             header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
             header('Content-Type: application/json');
